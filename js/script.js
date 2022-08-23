8faec9e21de9929ac278966a97b595b5f0f08cb9
alexNovik <alexnovik86@gmail.com>
2022-08-23 21:55:42 +0300

    fourth commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return (
+      isMobile.Android() ||
+      isMobile.BlackBerry() ||
+      isMobile.iOS() ||
+      isMobile.Opera() ||
+      isMobile.Windows()
+    );
+  },
+};
+
+let body = document.querySelector("body");
+
+if (isMobile.any()) {
+  body.classList.add("touch");
+  let arrows = document.querySelectorAll(".arrow");
+  for (i = 0; i < arrows.length; i++) {
+    let thisLink = arrows[i].previousElementSibling;
+    let subMenu = arrows[i].nextElementSibling;
+    let thisArrow = arrows[i];
+    thisLink.classList.add("parent");
+    arrows[i].addEventListener("click", function () {
+      subMenu.classList.toggle("open");
+    });
+  }
+} else {
+  body.classList.add("mouse");
+}
